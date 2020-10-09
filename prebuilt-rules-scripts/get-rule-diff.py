c7991fe9409c3897cf1b1a001ae7feefb6af9978
Ben Skelker <54019610+benskelker@users.noreply.github.com>
2020-10-09 15:09:11 +0300

    updated script to use rule_id and fixes 7.9.0 final file (#284)

--- prebuilt-rules-scripts/get-rule-diff.py
+++ prebuilt-rules-scripts/get-rule-diff.py
@@ -31,40 +31,44 @@ lasted_rules_dict = sorted(lasted_rules_dict, key=sort_by_name)
 
 previous_rules_dict = sorted(previous_rules_dict, key=sort_by_name)
 
-oldRuleNames = []
-newRuleNames = []
-    
+# oldRuleNames = []
+# newRuleNames = []
+oldName = None
 counter = 0 
 ruleFound = False
 for new_rule in lasted_rules_dict:
-    if new_rule['name'] not in newRuleNames:
-        newRuleNames.append(new_rule['name'])
+    # if new_rule['name'] not in newRuleNames:
+    #     newRuleNames.append(new_rule['name'])
     for old_rule in previous_rules_dict:
-        if old_rule['name'] not in oldRuleNames:
-            oldRuleNames.append(old_rule['name'])
-        if old_rule['name'] == new_rule['name']:
+        # if old_rule['name'] not in oldRuleNames:
+        #     oldRuleNames.append(old_rule['name'])
+        if old_rule['rule_id'] == new_rule['rule_id']:
+            if old_rule['name'] != new_rule['name']:
+                oldName = old_rule['name']
+            old_rule['name'] = new_rule['name']
             if 'changelog' in old_rule:
                 new_rule['changelog'] = old_rule['changelog']
             if old_rule['version'] != new_rule['version']:
-                new_rule['last_update'] = "7.9.0"
+                new_rule['last_update'] = "7.10.0"
                 if 'changelog' not in new_rule:
                     new_rule['changelog'] = {}
                     new_rule['changelog']['changes'] = []
                 if 'query' in new_rule:
                     if old_rule['query'] != new_rule['query']:
-                        new_rule['changelog']['changes'].append({"version": new_rule['version'], "updated": new_rule['last_update'], "pre_query": old_rule['query'], "doc_text": "Updated query."})
+                        new_rule['changelog']['changes'].append({"version": new_rule['version'], "updated": new_rule['last_update'], "pre_query": old_rule['query'], "doc_text": "Updated query.", "pre_name": oldName})
                     if old_rule['query'] == new_rule['query']:
-                        new_rule['changelog']['changes'].append({"version": new_rule['version'], "updated": new_rule['last_update'], "pre_query": old_rule['query'], "doc_text": "Formatting only."})
+                        new_rule['changelog']['changes'].append({"version": new_rule['version'], "updated": new_rule['last_update'], "pre_query": old_rule['query'], "doc_text": "Formatting only.", "pre_name": oldName})
                 if 'query' not in new_rule:
-                    new_rule['changelog']['changes'].append({"version": new_rule['version'], "updated": new_rule['last_update'], "pre_query": "N/A", "doc_text": "Formatting only."})
+                    new_rule['changelog']['changes'].append({"version": new_rule['version'], "updated": new_rule['last_update'], "pre_query": "N/A", "doc_text": "Formatting only.", "pre_name": oldName})
             else:
                 new_rule['last_update'] = old_rule['last_update']
             diff_dict.append(new_rule)
             new_rule['added'] = old_rule['added']
+            oldName = None
             ruleFound = True
     if ruleFound == False:
-        new_rule['last_update'] = "7.9.0"
-        new_rule['added'] = "7.9.0"
+        new_rule['last_update'] = "7.10.0"
+        new_rule['added'] = "7.10.0"
         diff_dict.append(new_rule)
         counter = counter + 1
     ruleFound = False
