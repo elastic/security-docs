18174d1672be68b89b54689467d405a4d8d03abe
Ben Skelker <54019610+benskelker@users.noreply.github.com>
2020-10-11 13:53:46 +0300

    updates scripts and 7.9.1 prebuilt rule changes (#290)
    # Conflicts:
    #	prebuilt-rules-scripts/create-json-from-docs.py
    #	prebuilt-rules-scripts/create_documentation.py
    #	prebuilt-rules-scripts/get-rule-diff.py
    #	prebuilt-rules-scripts/orig-rules-json-files/7.9.0-prebuilt-rule.json
    #	prebuilt-rules-scripts/orig-rules-json-files/7.9.1-prebuilt-rule.json
    #	prebuilt-rules-scripts/readme.md
    #	prebuilt-rules-scripts/update_current_text.py

---
+++ prebuilt-rules-scripts/update_current_text.py
@@ -0,0 +1,41 @@
+import json
+import textwrap
+import toml
+import os
+import glob
+from pathlib import Path
+import re
+
+releaseVersion = "7.9.1" #Security app release version - update as required
+
+def sort_by_name(rule):
+    '''
+    Helper to sort rule by name'''
+    return rule['name']
+    
+# Path to latest prebuilt rules JSON file
+
+with open("orig-rules-json-files/" + releaseVersion + "-prebuilt-rule.json", "r") as source:
+    currentText = json.load(source)
+
+# Path to JSON file generated from existing documentation
+    
+with open("diff-files/gen-files/json-from-docs-" + releaseVersion + ".json", "r") as source:
+    updatedText = json.load(source)
+
+for rule in currentText:
+    for newText in updatedText:
+        if rule['name'] == newText['name']:
+            rule['description'] = newText['description']
+            if 'false_positives' in newText and 'false_positives' in rule:
+                print(rule['name'])
+                rule['false_positives'][0] = newText['false_positives'][0]
+            if 'note' in newText:
+                rule['note'] = newText['note']
+
+# Output file with updated text from the documentation for previously existing
+# prebuilt rules. New rules are unchanged.
+
+with open("diff-files/gen-files/updated-text-json-file-" + releaseVersion + ".json", "w") as fp:
+    json.dump(currentText, fp, indent=2)
+            
